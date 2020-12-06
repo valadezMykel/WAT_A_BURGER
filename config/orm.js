7e80d6cfe16ffaaa7641d960af139abb2d980926
Mykel Valadez <valadezmykel@gmail.com>
2020-12-06 15:49:20 -0800

    removed err throwing on orm

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ const connection = require("./connection");
 
 exports.selectAll = (table, cb)=>{
     connection.query("SELECT * FROM ??", [table], (err, results)=>{
-        if(err) throw err;
+        // if(err) throw err;
         cb(results);
     }); 
 
@@ -11,7 +11,7 @@ exports.selectAll = (table, cb)=>{
 exports.insertOne = (table, col, value, cb)=>{
     console.log (value)
     connection.query("INSERT INTO ?? (??) VALUES (?)", [table, col, value], (err, results)=>{
-        if(err) throw err;
+        // if(err) throw err;
         console.log("insert results", results);
         cb();
     });
@@ -19,7 +19,7 @@ exports.insertOne = (table, col, value, cb)=>{
 
 exports.updateOne = (table, col, changeValueTo, id, cb)=>{
     connection.query("UPDATE ?? SET ??=? WHERE id=?", [table, col, changeValueTo, id], (err, results)=>{
-        console.log(err);
+        // console.log(err);
         console.log(results);
         cb();
     });
